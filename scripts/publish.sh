312fac642ecde0e91ee5248239f344007a92765d
lexicalunit <lexicalunit@lexicalunit.com>
2020-05-27 10:51:27 -0700

    More visibility in publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,6 +8,15 @@ usage() {
     exit 1
 }
 
+run() {
+    echo -n $'\e[38;5;40m$\e[38;5;63m '
+    echo -n "$@"
+    echo $'\e[0m'
+    eval "$@"
+    return $?
+}
+
+
 if echo "$*" | grep -Eq -- '--help\b|-h\b' || [[ -z $1 ]]; then
     usage
 fi
@@ -31,28 +40,28 @@ if [[ -n "$CHANGES" ]]; then
 fi
 
 # bump the version in pyproject.toml
-poetry version "$KIND"
+run "poetry version '$KIND'"
 
 # run tests to ensure the build is good
-poetry run pytest
+run "poetry run pytest"
 
 # fetch the version from pyproject.toml
 VERSION="$(grep "^version" < pyproject.toml | cut -d= -f2 | sed 's/"//g;s/ //g;s/^/v/;')"
 
 # build the release
-poetry build
+run "poetry build"
 
 # commit changes
-git commit -am "Release $VERSION"
+run "git commit -am 'Release $VERSION'"
 
 # publish the release; assumes you've set up non-interactive publishing by
 # previously having run: `poetry config pypi-token.pypi "YOUR-PYPI-TOKEN-GOES-HERE"`
 if ! poetry publish -n; then
     echo "error: publish command failed, see log for details" 1>&2
-    git reset --hard HEAD~1
+    run "git reset --hard HEAD~1"
     exit 1
 fi
 
 # tag and push changes to origin/master
-git tag "$VERSION"
-git push --tags origin master
+run "git tag '$VERSION'"
+run "git push --tags origin master"
